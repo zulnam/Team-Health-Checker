9ea03f3f312d1ed75c2651cb2ec47b22579ae6db
Valentin Necsescu <bigv@valentins-macbook-pro.home>
2021-07-01 12:58:44 +0200

    fix: tagName TypeError caused by global styles in Head

--- packages/head/HeadIndex.tsx
+++ packages/head/HeadIndex.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
+import globalStyles from './globalStyles';
+import { Global } from '@emotion/core';
 
 function HeadIndex() {
   return (
-    <Head>
-      <title>Team Health Checker</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link
-        href="https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap"
-        rel="stylesheet"
-      />
-      <style jsx global>{`
-        * {
-          font-family: 'Ubuntu Mono', monospace;
-          background-color: black;
-          color: white;
-        }
-        body {
-          padding: 0;
-          font-family: monospace;
-        }
-      `}</style>
-    </Head>
+    <>
+      <Head>
+        <title>Team Health Checker</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Global styles={globalStyles} />
+    </>
   );
 }
 
---
+++ packages/head/globalStyles.ts
@@ -0,0 +1,15 @@
+import css from '@emotion/css';
+
+const globalStyles = () => css`
+  * {
+    font-family: 'Ubuntu Mono', monospace;
+    background-color: black;
+    color: white;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+export default globalStyles;
