9b92ba1da804da0e043ca3bb72d14dbc41c849f2
Valentin Necsescu <vlostek@gmail.com>
2021-07-05 13:23:21 +0200

    tests: added Headindex test

---
+++ packages/head/__tests__/HeadIndex.test.js
@@ -0,0 +1,31 @@
+import { render, debug, waitFor } from '@testing-library/react';
+import { shallow } from 'enzyme';
+import HeadIndex from '../HeadIndex';
+
+describe('head metadata', () => {
+  it('loads all metadata on render', async () => {
+    const wrapper = shallow(<HeadIndex />);
+
+    // title tag
+    expect(
+      wrapper.containsMatchingElement(<title>Team Health Checker</title>)
+    ).toBe(true);
+
+    // viewport meta tag
+    expect(
+      wrapper.containsMatchingElement(
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      )
+    ).toBe(true);
+
+    // link tag
+    expect(
+      wrapper.containsMatchingElement(
+        <link
+          href="https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap"
+          rel="stylesheet"
+        />
+      )
+    ).toBe(true);
+  });
+});
