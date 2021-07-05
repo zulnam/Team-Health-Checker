e685300c98ed9e0fb93dfb6f5ab40bd108e110cd
Valentin Necsescu <vlostek@gmail.com>
2021-07-05 12:53:59 +0200

    build: modified jest config, updated snapshots

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { defaults } = require('jest-config');
 module.exports = {
+  testEnvironment: 'jsdom',
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'js', 'ts', 'tsx'],
   setupFilesAfterEnv: ['./enzyme.js'],
   coverageReporters: ['text', 'html'],
