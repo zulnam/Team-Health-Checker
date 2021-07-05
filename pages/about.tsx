25b0ce7301c47b8d552d41c05b01233f3e3840aa
Valentin Necsescu <vlostek@gmail.com>
2021-07-05 10:55:19 +0200

    fix: paragraph having list child

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,14 +8,14 @@ const About = () => (
       <p>
         Welcome to the Team Health Checker tool. Please see the table of
         contents below for further information.
-        <ol>
-          <li>Initiating a room</li>
-          <li>Using a room as an admin</li>
-          <li>Using a room as a user</li>
-          <li>Viewing reports</li>
-          <li>Exporting reports</li>
-        </ol>
       </p>
+      <ol>
+        <li>Initiating a room</li>
+        <li>Using a room as an admin</li>
+        <li>Using a room as a user</li>
+        <li>Viewing reports</li>
+        <li>Exporting reports</li>
+      </ol>
     </div>
   </MainContainer>
 );
